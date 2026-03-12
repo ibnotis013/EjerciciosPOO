93f270bcb612245ac503ac1858ee611bd3e2a9c3
ibnotis013 <n.ledo@liceolapaz.net>
2026-03-12 11:55:54 +0100

    ejercicios 7 y 8 terminador. TODOS FINALIZADOS

--- src/ejercicio7/Principal.java
+++ src/ejercicio7/Principal.java
@@ -1,12 +1,32 @@
 package ejercicio7;
 
 /*
-* Haz un método que reciba la lista de cadenas que se muestra a continuación y, pasándole una letra,
-*           nos cuente cuántas veces aparece esa letra en cada palabra
-* */
+ * Haz un método que reciba la lista de cadenas que se muestra a continuación y, pasándole una letra,
+ *           nos cuente cuántas veces aparece esa letra en cada palabra
+ * */
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Principal {
+    static ArrayList<String> lista = new ArrayList<String>(Arrays.asList("Nicolás", "Francisco", "Hugo", "Lucas", "Ruben", "Iker", "Sofia", "Jose Luis", "Diego", "Rico", "Amanda", "Francisca"));
+
     public static void main(String[] args) {
 
+        devolverLista("a");
+    }
+
+    public static ArrayList<Integer> devolverLista(String letra) {
+        ArrayList<Integer> numeroLetras = new ArrayList<>();
+        for (String st : lista) {
+            int acumulador = 0;
+            for (int i = 0; i < st.length(); i++) {
+                if (letra.equalsIgnoreCase(String.valueOf(st.charAt(i)))) {
+                    acumulador++;
+                }
+            }
+            numeroLetras.add(acumulador);
+        }
+        return numeroLetras;
     }
 }
--- src/ejercicio8/Principal.java
+++ src/ejercicio8/Principal.java
@@ -5,8 +5,31 @@ package ejercicio8;
  *           nos cuente cuántas veces aparece esa letra en cada posición de la lista por consola
  * */
 
-public class Principal {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Principal {
+    static ArrayList<String> lista = new ArrayList<String>(Arrays.asList("Nicolás", "Francisco", "Hugo", "Lucas", "Ruben", "Iker", "Sofia", "Jose Luis", "Diego", "Rico", "Amanda", "Francisca"));
+
+    public static void main(String[] args) {
+    devolverLista("a");
+    }
+
+    public static void devolverLista(String letra) {
+        ArrayList<Integer> numeroLetras = new ArrayList<>();
+        for (String st : lista) {
+            System.out.println("estudiando la palabra "+st);
+            int contador = 0;
+            for (int i = 0; i < st.length(); i++) {
+                if (letra.equalsIgnoreCase(String.valueOf(st.charAt(i)))) {
+                    System.out.println("La letra "+letra+" esta en la posicion "+(i+1));
+                    contador++;
+                }
+            }
+            if(contador==0){
+                System.out.println("El nombre"+st+" no tiene la letra "+letra);
+            }
+
+        }
     }
 }
